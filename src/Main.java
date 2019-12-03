5cae57ae55358bfe3005d52a2bbe20afbe249d27
kfir147 <kfir.rn@gmail.com>
2019-12-03 21:17:31 +0200

    Key verification of 8 characters long. (not more and not less)

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
-        System.out.println("~~~~~~~~~~~~~~~~~~~ Let's encrypt using DES! ~~~~~~~~~~~~~~~~~~~\nPlease enter the Key:");
+        System.out.println("~~~~~~~~~~~~~~~~~~~ Let's encrypt using DES! ~~~~~~~~~~~~~~~~~~~\n\nPlease enter a valid Key (with a size of 8 characters):");
         String key = userInput.nextLine();
-        System.out.println("Please enter the Words:");
-        String msg = userInput.nextLine();
-        DES des = new DES();
+        if (key.length() == 8){
+            System.out.println("Please enter the Words:");
+            String msg = userInput.nextLine();
+            DES des = new DES();
 
-        String encrypted_msg = des.Cipher(msg, key, 1);
-
-        des.Cipher(encrypted_msg, key, 2);
+            String encrypted_msg = des.Cipher(msg, key, 1);
+            des.Cipher(encrypted_msg, key, 2);
+        }
+        else
+            System.out.println("The key you entered is not valid (the size of it is " + key.length() + " characters).");
     }
 }
 
